34d046d0c7fa72c998bc96b92e2315b19d663b91
LucianoMartLera <tu_correo@example.com>
2026-06-19 01:01:52 -0300

    Se agregaron las api con proyectos, servicios , se corrigieron los errores de la entrega pasada y el json con datos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,9 +5,11 @@ import { Products } from './pages/products/products';
 import { Contact } from './pages/contact/contact';
 
 export const routes: Routes = [
-    {path: '', component:Home },
-    {path: 'nosotros', component:About },
-    {path: 'productos', component:Products},
-    {path: 'contacto', component:Contact},
-    {path: '**', redirectTo: ''}
+  { path: 'inicio', component: Home },
+  { path: 'nosotros', component: About },
+  { path: 'productos', component: Products },
+  { path: 'contacto', component: Contact },
+
+  { path: '', redirectTo: 'inicio', pathMatch: 'full' },
+  { path: '**', redirectTo: 'inicio' }
 ];
